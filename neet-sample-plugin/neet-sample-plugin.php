c3e820c52f69dcf7db64dbc8ee99a6974d145c16
neet-no1 <no1.neet.professional@gmail.com>
2021-01-06 21:48:30 +0900

    サンプルプラグイン作成

---
+++ neet-sample-plugin/neet-sample-plugin.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       neet-sample-plugin
+ * Plugin URI:        https://neet-professional.com/
+ * Description:       create plugin sample
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            TON(Top of neet)
+ * Author URI:        https://github.com/neet-no1
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       neet-sample-plugin
+ * Domain Path:       /languages
+ */
